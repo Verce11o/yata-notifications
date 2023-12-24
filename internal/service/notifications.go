73225234e29337587a5ecccc51674f6b6c709aee
Verce11o <taymas_rashitov@mail.ru>
2023-12-24 23:01:52 +0600

    interface implementation (placeholder)

--- internal/service/notifications.go
+++ internal/service/notifications.go
@@ -1,7 +1,9 @@
 package service
 
 import (
+	"context"
 	"github.com/Verce11o/yata-notifications/internal/repository"
+	pb "github.com/Verce11o/yata-protos/gen/go/notifications"
 	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 )
@@ -14,4 +16,19 @@ type NotificationsService struct {
 
 func NewNotificationsService(log *zap.SugaredLogger, tracer trace.Tracer, repo repository.Repository) *NotificationsService {
 	return &NotificationsService{log: log, tracer: tracer, repo: repo}
+}
+
+func (n NotificationsService) SubscribeToUser(ctx context.Context, request *pb.SubscribeToUserRequest) error {
+	ctx, span := n.tracer.Start(ctx, "notificationService.SubscribeToUser")
+	defer span.End()
+
+	return nil
+
+}
+
+func (n NotificationsService) UnSubscribeFromUser(ctx context.Context, request *pb.UnSubscribeFromUserRequest) error {
+	ctx, span := n.tracer.Start(ctx, "notificationService.UnSubscribeFromUser")
+	defer span.End()
+
+	return nil
 }
