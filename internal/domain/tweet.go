69f7f7d67d9985eb1b1290c41f5c7f518efc327f
Verce11o <taymas_rashitov@mail.ru>
2023-12-28 23:27:49 +0600

    migration

--- internal/domain/tweet.go
+++
@@ -1,6 +0,0 @@
-package domain
-
-type IncomingNewTweetNotification struct {
-	FromUserID string `json:"from_user_id"`
-	ShortTitle string `json:"short_title"`
-}
---
+++ migrations/20231228204508_create_notifications_table.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS notifications (
+    notification_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID NOT NUlL,
+    sender_id UUID NOT NULL,
+    read bool default false,
+    created_at   TIMESTAMP WITH TIME ZONE    NOT NULL DEFAULT NOW()
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+-- +goose StatementEnd
